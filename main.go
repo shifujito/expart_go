2dba5d0e7d545f4cba3084df783db624a5bebd4f
shifujito <fujito.shion@gmail.com>
2023-09-03 20:49:22 +0900

    メソッド色

--- main.go
+++ main.go
@@ -2,27 +2,16 @@ package main
 
 import "fmt"
 
-type Chip struct {
-	Number int
-}
+type Hex int
 
-type Card struct {
-	string
-	Chip
-	Number int
-}
-
-func (c *Chip) Scan() {
-	fmt.Println(c.Number)
+func (h Hex) String() string {
+	return fmt.Sprintf("%x", int(h))
 }
 
 func main() {
-	c := Card{
-		string: "Credit",
-		Chip: Chip{
-			Number: 1212,
-		},
-		Number: 5454,
-	}
-	c.Scan()
+	fv := Hex(1024).String
+	fmt.Println(fv())
+
+	fe := Hex.String
+	fmt.Println(fe(1024))
 }
