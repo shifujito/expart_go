c08bf70110834325edaa6c4b9c30bdc02dccf489
shifujito <fujito.shion@gmail.com>
2023-09-19 02:30:48 +0900

    struct

--- main.go
+++ main.go
@@ -2,17 +2,14 @@ package main
 
 import "fmt"
 
+type person struct {
+	name string
+	age  int
+	pet  string
+}
+
 func main() {
-	m := map[string]int{
-		"hello": 5,
-		"world": 0,
-	}
-	v, ok := m["hello"]
-	fmt.Println(v, ok) // 5, true
-
-	v, ok = m["world"]
-	fmt.Println(v, ok) // 0, true
-
-	v, ok = m["hoge"]
-	fmt.Println(v, ok) // 0, false
+	bob := person{}
+	fmt.Println(bob)
+	// fmt.Println(bob == nil)
 }
